6e4bea8764432e230964d717448f65392c994ad8
AMSeify <amh.seify@gmail.com>
2022-05-02 20:12:17 +0430

    First Task (make Backtester)

--- BackTester.py
+++ BackTester.py
@@ -1,6 +1,3 @@
-from ast import Str
-
-
 class Tester:
     """this Class get pandas DataFram and Backtest the strategy
         Clearly make the winrate by persent
@@ -21,5 +18,48 @@ class Tester:
         self.df = dataframe
         self.BuyCol = BuyCol
         self.SellCol = SellCol
+        self.money = 100
+        
+    def persentBacktester(self):
+        persent = self.money
+        buy = False
+        sell = False
+        
+        for index in self.df.index:
+            if (self.df.Buy[index] == True) & (buy == False):
+                s= self.df.Close[index]
+                buy =True
+                
+            elif (self.df.Buy[index] == False) & (buy == True):
+                persent *= 1 +((self.df.Close[index]-s)/s)
+                buy = False
+                
+            elif (self.df.Sell[index] == True) & (sell == False):
+                s= self.df.Close[index]
+                sell =True
+                
+            elif (self.df.Sell[index] == False) & (sell == True):
+                persent *= 1 +(-1* (self.df.Close[index]-s)/s)
+                sell = False
+                
+        return persent
 
+
+# first test of the Backtest Proccess
+# The EMA Algorithm
+
+import yfinance as yf
+import pandas_ta as ta
+
+ETH = yf.download(tickers="ETH-USD" , interval="15m" , period="35d")
+ETH["EMA5"] = ta.ema(ETH.Close , 5)
+ETH["EMA8"] = ta.ema(ETH.Close , 8)
+ETH["EMA13"] = ta.ema(ETH.Close , 13)
+ETH.dropna(inplace=True)
+
+ETH["Buy"] = (ETH.EMA5 > ETH.EMA8) & (ETH.EMA8 > ETH.EMA13)
+ETH["Sell"] = (ETH.EMA5 < ETH.EMA8) & (ETH.EMA8 < ETH.EMA13)
+
+ETH_tester = Tester(ETH , "Buy" , "Sell")
+print( ETH_tester.persentBacktester())
 
