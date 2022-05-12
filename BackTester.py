78f4acd3d555a2e80ef7b4aad134a944b3401c76
AMSeify <amh.seify@gmail.com>
2022-05-12 20:57:02 +0430

    Secondary Change (there is a lot to ging on :)

--- BackTester.py
+++ BackTester.py
@@ -4,7 +4,7 @@ class Tester:
     """this Class get pandas DataFrame and Backtest the strategy
         Clearly make the winrate by persent
     """
-    def __init__(self , dataframe , BuyCol , SellCol) -> None:
+    def __init__(self , dataframe ) -> None:
         
         """import the pandas DF and clear which columns is
             Buy/Sell Open and which one is Buy/Sell Stops
@@ -18,8 +18,6 @@ class Tester:
             SellCol (Str): name of the Buy Column
         """
         self.df = dataframe
-        self.BuyCol = BuyCol
-        self.SellCol = SellCol
         self.money = 100
         self.BadRecords = pd.DataFrame.from_dict({
             "time": [],
@@ -27,7 +25,14 @@ class Tester:
             "position" : []
         })
         
-    def persentBacktester(self):
+    def persentBacktester(self , BuyCol , SellCol):
+        """this method Calculate the strategy profit by persent
+
+        Returns:
+            float64: total persent of the strategy %
+        """
+        self.BuyCol = BuyCol
+        self.SellCol = SellCol
         persent = self.money
         buy = False
         sell = False
@@ -49,9 +54,14 @@ class Tester:
                 persent *= 1 +(-1* (self.df.Close[index]-s)/s)
                 sell = False
                 
-        return persent
+        return persent -100
     
     def badrecords(self):
+        """this Method sent the worst position to debug the strategy
+
+        Returns:
+            pandas.dataframe: this Dataframe include worst trade for backtest
+        """
         br = self.BadRecords
         # persent = 1
         buy = False
@@ -84,25 +94,55 @@ class Tester:
                                 }
                 sell = False
         return br
+    
+    def SigMixer(self, ins, start, end):
+        """this method make Clear signal from boolean multiple Series
 
+        Args:
+            ins (pandas.Series): this is simple instance signal and instance signal must be
+            always true to continue the trade
+            start (pandas.Series): when the start series rich to True signal will start :)
+            end (pandas.Series): when the stop series rich to True the main signal will stops
 
-# first test of the Backtest Proccess
-# The EMA Algorithm
+        Returns:
+            pandas.Series: the main signal
+        """
+        startindex = None
+        endindex = None 
+        Is_StartFound = False
+        self.df["sig"] = False
+        signal = self.df.sig
+        for index in ins.index:
+            # print(start[index])
+            if ins[index] & start[index] & ~Is_StartFound:
+                startindex = index
+                Is_StartFound = True
+            elif ins[index] & end[index]:
+                endindex = index
+        signal[startindex:endindex] = True
+        return signal
 
 import yfinance as yf
 import pandas_ta as ta
 
-ETH = yf.download(tickers="ETH-USD" , interval="5m" , period="30d")
-ETH["EMA5"] = ta.ema(ETH.Close , 5)
-ETH["EMA8"] = ta.ema(ETH.Close , 8)
-ETH["EMA13"] = ta.ema(ETH.Close , 13)
+ETH = yf.download(tickers="XRP-USD" , interval="5m" , period="25h")
+ETH["EMA10"] = ta.ema(ETH.Close , 10)
+ETH["EMA15"] = ta.ema(ETH.Close , 15)
+ETH["EMA20"] = ta.ema(ETH.Close , 20)
+
 ETH.dropna(inplace=True)
 
-ETH["Buy"] = (ETH.EMA5 > ETH.EMA8) & (ETH.EMA8 > ETH.EMA13)
-ETH["Sell"] = (ETH.EMA5 < ETH.EMA8) & (ETH.EMA8 < ETH.EMA13)
+ETH["Buy"] = (ETH.EMA5 > ETH.EMA8) & (ETH.EMA8 > ETH.EMA13) #& (ETH.MACD > 0 )
+ETH["Sell"] = (ETH.EMA5 < ETH.EMA8) & (ETH.EMA8 < ETH.EMA13) #& (ETH.MACD < 0 )
 
-ETH_tester = Tester(ETH , "Buy" , "Sell")
-# print(ETH_tester.BadRecords)
-ETH_tester.badrecords().sort_values("profit").to_csv("BadRecords.csv")
+ETH_tester = Tester(ETH)
 
-ETH[(ETH.Buy==True) | (ETH.Sell == True)].to_csv("ETH5min_3eyes.csv")
+ETH["MACD"] = ta.macd(ETH.Close ,12,26,9)["MACDh_12_26_9"] > 0
+# print(type(stop))
+ETH["FinalBuy"] = ETH_tester.SigMixer(ETH.Buy , ETH.MACD ,~ETH.Buy)
+ETH["FinalSell"] = ETH_tester.SigMixer(ETH.Sell , ~ETH.MACD ,~ETH.Sell)
+# print(ETH.FinalBuy)
+
+print(ETH_tester.persentBacktester("Buy" , "Sell"))
+
+print(ETH_tester.persentBacktester("FinalBuy" , "FinalSell"))
